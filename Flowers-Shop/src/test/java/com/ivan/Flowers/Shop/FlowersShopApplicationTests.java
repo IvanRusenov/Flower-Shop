3561e5a5a6bb6f289c56fece7698a3a6be644e05
IvanRusenov <i_rusenov@abv.bg>
2024-05-29 09:38:11 +0200

    Initial commit

---
+++ Flowers-Shop/src/test/java/com/ivan/Flowers/Shop/FlowersShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivan.Flowers.Shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlowersShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
