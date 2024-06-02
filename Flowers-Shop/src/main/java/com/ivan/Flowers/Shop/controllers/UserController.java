a7c9fb8b8312e6194ed268fd640348e292c7b359
IvanRusenov <i_rusenov@abv.bg>
2024-06-02 09:53:48 +0200

    Create register.html

--- Flowers-Shop/src/main/java/com/ivan/Flowers/Shop/controllers/UserController.java
+++ Flowers-Shop/src/main/java/com/ivan/Flowers/Shop/controllers/UserController.java
@@ -1,7 +1,9 @@
 package com.ivan.Flowers.Shop.controllers;
 
+import com.ivan.Flowers.Shop.models.dtos.UserRegisterDTO;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -10,5 +12,10 @@ public class UserController {
     @GetMapping("/")
     public ModelAndView index() {
         return new ModelAndView("index");
+    }
+
+    @GetMapping("/register")
+    public ModelAndView register(@ModelAttribute("userRegisterDTO") UserRegisterDTO userRegisterDTO) {
+        return new ModelAndView("register");
     }
 }
