4052134a7a3583d281092d31edbf8720fb15285a
IvanRusenov <i_rusenov@abv.bg>
2024-06-14 12:45:09 +0200

    Update CartItem.java

--- Flowers-Shop/src/main/java/com/ivan/Flowers/Shop/models/entities/CartItem.java
+++ Flowers-Shop/src/main/java/com/ivan/Flowers/Shop/models/entities/CartItem.java
@@ -1,7 +1,16 @@
 package com.ivan.Flowers.Shop.models.entities;
 
+import com.ivan.Flowers.Shop.models.entities.Bouquet;
+import jakarta.persistence.*;
+
+@Entity(name = "cart_items")
 public class CartItem {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @ManyToOne
     private Bouquet bouquet;
     private int quantity;
 
@@ -20,5 +29,13 @@ public class CartItem {
 
     public void setQuantity(int quantity) {
         this.quantity = quantity;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
     }
 }
