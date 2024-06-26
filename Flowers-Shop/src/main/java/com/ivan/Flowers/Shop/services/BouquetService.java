b46c8822e4caf18f19d340c8b724f93fec46878f
IvanRusenov <i_rusenov@abv.bg>
2024-06-26 12:01:00 +0200

    check

--- Flowers-Shop/src/main/java/com/ivan/Flowers/Shop/services/BouquetService.java
+++ Flowers-Shop/src/main/java/com/ivan/Flowers/Shop/services/BouquetService.java
@@ -1,7 +1,11 @@
 package com.ivan.Flowers.Shop.services;
 
 import com.ivan.Flowers.Shop.models.dtos.BouquetDTO;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 public interface BouquetService {
-    boolean addBouquet(BouquetDTO bouquetDTO);
+
+    boolean addBouquet(BouquetDTO bouquetDTO) throws IOException;
 }
