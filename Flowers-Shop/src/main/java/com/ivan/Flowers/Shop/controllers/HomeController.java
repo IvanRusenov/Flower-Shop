7c337a9afef50642beba94653afe3de4a59ac800
IvanRusenov <i_rusenov@abv.bg>
2024-07-28 13:34:02 +0200

    Create HomeControllerIT

--- Flowers-Shop/src/main/java/com/ivan/Flowers/Shop/controllers/HomeController.java
+++ Flowers-Shop/src/main/java/com/ivan/Flowers/Shop/controllers/HomeController.java
@@ -1,13 +1,9 @@
 package com.ivan.Flowers.Shop.controllers;
 
-import com.ivan.Flowers.Shop.models.dtos.BouquetDTO;
-import com.ivan.Flowers.Shop.models.entities.Bouquet;
 import com.ivan.Flowers.Shop.services.HomeService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
-
-import java.util.List;
 
 @Controller
 public class HomeController {
