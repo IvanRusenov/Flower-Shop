3223efc4073de7575152b92f871a4eb35f416b00
IvanRusenov <i_rusenov@abv.bg>
2024-07-29 16:32:10 +0200

    Create Scheduler

--- Flowers-Shop/src/main/java/com/ivan/Flowers/Shop/FlowersShopApplication.java
+++ Flowers-Shop/src/main/java/com/ivan/Flowers/Shop/FlowersShopApplication.java
@@ -2,8 +2,10 @@ package com.ivan.Flowers.Shop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class FlowersShopApplication {
 
 	public static void main(String[] args) {
---
+++ Flowers-Shop/src/main/java/com/ivan/Flowers/Shop/scheduler/Scheduler.java
@@ -0,0 +1,25 @@
+package com.ivan.Flowers.Shop.scheduler;
+
+import com.ivan.Flowers.Shop.enums.StatusType;
+import com.ivan.Flowers.Shop.services.impls.OrderServiceImpl;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Scheduler {
+
+    private final OrderServiceImpl orderService;
+
+    public Scheduler(OrderServiceImpl orderService) {
+        this.orderService = orderService;
+    }
+
+    @Scheduled(cron = "1 * * * * *")
+    public void deleteOrdersWithStatusCanceled(){
+
+        orderService.getAllOrdersDesc().stream()
+               .filter(o -> o.getStatus().equals(StatusType.CANCELED))
+               .forEach(o-> orderService.delete(o.getId()));
+        System.out.println();
+    }
+}
