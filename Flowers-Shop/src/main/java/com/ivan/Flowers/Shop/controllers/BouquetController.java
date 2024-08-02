38b313b74d52d0e533b1f914f3136c689fcb0f57
IvanRusenov <i_rusenov@abv.bg>
2024-08-02 15:36:41 +0200

    Update bouquet-add.html

--- Flowers-Shop/src/main/java/com/ivan/Flowers/Shop/controllers/BouquetController.java
+++ Flowers-Shop/src/main/java/com/ivan/Flowers/Shop/controllers/BouquetController.java
@@ -5,9 +5,7 @@ import com.ivan.Flowers.Shop.services.BouquetService;
 import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.io.IOException;
